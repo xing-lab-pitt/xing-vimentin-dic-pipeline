d5497c608a1c83c76e958de51b8ae6482a6fd2a2
Thomas Hyatt <tch42@pitt.edu>
2022-03-15 17:42:36 -0400

    changed pca scripts

--- scripts/3-compute/pipe_11_vim_haralick_pca.py
+++ scripts/3-compute/pipe_11_vim_haralick_pca.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
 # In[1]: import
 import sys
 sys.path.insert(1, '/home/thomas/research/projects/emt/scripts/memes/')
@@ -29,32 +27,16 @@ import pandas as pd
 
 from sklearn import decomposition, cluster, manifold
 from sklearn.neighbors import KernelDensity
-from sklearn.preprocessing import StandardScaler
-from cell_class import single_cell
-import contour_class
-import utility_tools
-
-import image_warp
-from contour_tool import df_find_contour_points, find_contour_points, generate_contours, align_contour_to, align_contours
+from sklearn.preprocessing import StandardScaler, RobustScaler, Normalizer
+from cell_class import single_cell, fluor_single_cell
 import pipe_util2
 
-# In[2]:
-
-
-# main_path='/home/zoro/Desktop/experiment_data/2019-03-22_a549_tgf4ng_2d/'
-# main_path = '/mnt/data0/Ke/weikang_exp_process/2019-05-05_A549_vim/'
-# cells_path=main_path+'fluor_cells/'
-# posi_start=1
-# posi_end=1
-
-
-# do not use StandarScaler on cell contour points
-# ----------cal cell_contour pca coordinates-------------------
-def morph_pca(all_datset_path, all_datsets, pattern='XY'):
+# In[2]: function
+def vim_haralick_pca(all_datset_path,all_datsets,norm,pattern='XY'):
     """
 
-    :param all_datset_path: string, including several output folders
-    :param pattern: string, for indexing output folders
+    :param all_datset_path: folder including all output folders
+    :param pattern: the pattern for matching the output folders
     :return:
     """
     for datset_idx in range(len(all_datsets)):
@@ -62,16 +44,20 @@ def morph_pca(all_datset_path, all_datsets, pattern='XY'):
         curr_datset_path = pipe_util2.folder_verify(all_datset_path+all_datsets[datset_idx])
         output_path_list = pipe_util2.folder_file_num(curr_datset_path, pattern)
         i = 0
-        while i<len(output_path_list):
+        while i < len(output_path_list):
             output_path = output_path_list[i]
             output_path = pipe_util2.folder_verify(output_path)
             cells_path = output_path + "cells/"
 
-            with open(cells_path + 'cells', 'rb') as fp:
+            with open(cells_path + 'pcna_cells-02', 'rb') as fp:
                 cells = pickle.load(fp)
 
-            data = np.array([single_cell.cell_contour.points for single_cell in cells if hasattr(
-                single_cell, 'cell_contour')])
+            if norm == False:
+                data = np.array([single_cell.vimentin_feature_values[3]
+                    for single_cell in cells if hasattr(single_cell, 'vimentin_feature_values')])
+            else:
+                data = np.array([single_cell.vimentin_feature_values[4]
+                    for single_cell in cells if hasattr(single_cell, 'vimentin_feature_values')])
 
             if all_data.size == 0:
                 all_data = data
@@ -79,44 +65,53 @@ def morph_pca(all_datset_path, all_datsets, pattern='XY'):
                 all_data = np.vstack((all_data, data))
             i = i+1
 
-        # print(all_data.shape)
-        # mean = all_data.mean(axis = 0)
-        X = all_data
-        X, data_point_shape = utility_tools.flatten_data(X)
-        X = X.astype(np.float)
+        scaler = StandardScaler()
+
+        X = scaler.fit_transform(all_data)
         print(X.shape)
 
         pca = decomposition.PCA(n_components=0.98, svd_solver='full')
         Y = pca.fit_transform(X)
-        print(pca.explained_variance_ratio_, sum(pca.explained_variance_ratio_))
+        print(pca.components_, pca.explained_variance_ratio_)
 
+        # dot_color=np.arange(Y[:].shape[0])
+        # cm=plt.cm.get_cmap('jet')
+
+        # sc=plt.scatter(Y[:,0],Y[:,1],c=dot_color,cmap=cm)
         # plt.scatter(Y[:,0],Y[:,1],s=0.1)
-        # plt.xlabel('PC1')
-        # plt.ylabel('PC2')
+        # #plt.axis([-100000,500000,-2000,2000])
+        # plt.savefig("vim_haralic_1.png",dpi = 300)
         # plt.show()
+        if norm == False:
+            with open(all_datset_path+'vimentin_haralick_pca', 'wb') as fp:
+                pickle.dump(pca, fp)
+            fluor_feature_name = 'vimentin_haralick'
+        else:
+            with open(all_datset_path+'norm_vimentin_haralick_pca', 'wb') as fp:
+                pickle.dump(pca, fp)
+            fluor_feature_name = 'norm_vimentin_haralick'
 
-        with open(curr_datset_path + 'morph_pca', 'wb') as fp:
-            pickle.dump(pca, fp)
-
-
-        # do not use StandarScaler on cell contour points
-        # ----------cal cell_contour pca coordinates-------------------
         for output_path in output_path_list:
             output_path = pipe_util2.folder_verify(output_path)
-            cells_path = output_path+"cells/"
+            cells_path = output_path + "cells/"
 
             with open(cells_path + 'pcna_cells-02', 'rb') as fp:
                 cells = pickle.load(fp)
+
             for i in range(len(cells)):
-                if hasattr(cells[i], 'cell_contour'):
-                    data = np.expand_dims(cells[i].cell_contour.points, axis=0)
-                    X, X_shape = utility_tools.flatten_data(data)
+                if hasattr(cells[i], 'vimentin_feature_values'):
+                    if norm == False:
+                        X = np.expand_dims(cells[i].vimentin_feature_values[3], axis=0)
+                    else:
+                        X = np.expand_dims(cells[i].vimentin_feature_values[4], axis=0)
+                    X = scaler.transform(X)
                     Y = pca.transform(X)[0]
-                    cells[i].set_pca_cord(Y)
+                    cells[i].set_fluor_pca_cord(fluor_feature_name, Y)
             print('dumping '+cells_path+'pcna_cells-02')
             with open(cells_path + 'pcna_cells-02', 'wb') as fp:
                 pickle.dump(cells, fp)
 
+# In[3]: run
 if __name__ == "__main__":
     # stuff only to run when not called via 'import' here
     all_datset_path = '/home/thomas/research/projects/emt/data/out/pcna/' # directory containing all outputs of all datasets
@@ -124,6 +119,7 @@ if __name__ == "__main__":
                 '01-27-22_72hr_no-treat','02-03-22_72hr_no-treat',\
                 '02-11-22_72hr_no-treat','02-21-22_72hr_no-treat',\
                 '12-19-21_72hr_no-treat'] # directory names of all datasets
-    morph_pca(all_datset_path,all_datsets)
+    norm = False # whether to use normalized haralick values
+    vim_haralick_pca(all_datset_path, all_datsets,norm)
 
 # %%
