8f49895219549d2e66f94fd983c4defe86650462
Dante Poe <dap182@pitt.edu>
2022-06-09 14:48:08 -0400

    added new slurm array scripts

---
+++ src/3-compute/slurm_scripts/generate_slurm_array_file.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+#makes the slurm output folder and slurm array submission file 
+### THIS VERSION ONLY CAN MAKE ONE SLURM SUBMISSION FILE SO IF DIFFERENT RUNS OF THE SAME DATA WANTS TO BE RUN, THIS FILE NEEDS TO BE CHANGED.
+### ALL SLURM FILES WILL BE OVERWRITTEN EACH RUN
+
+### THIS VERSION ONLY CAN MAKE ONE SLURM SUBMISSION FILE SO IF DIFFERENT RUNS OF THE SAME DATA WANTS TO BE RUN, THIS FILE NEEDS TO BE CHANGED.
+### THIS ALSO ASSUMES THAT ALL POSITIONS ARE DONE WITH 0-2 BEFORE RUNNING THE STEPS 3-9. I BELIEVE IT WOULD START ANALZYING IMCOMPLETE SET OF AN XY POSITION
+	### IF RUN BEFORE PREVIOUS STEPS ARE COMPLETE
+
+source scripts/3-compute/slurm_config.sh
+
+####################
+#  SLURM SECTION   #
+####################
+
+#get name of experiment for naming slurm files
+desc=${data_dir##*/}
+
+#defining slurm output folder and slurm array submission file
+slurm_dir=${data_dir}/slurm 
+slurm_array_file=slurm_array_file_${pipeline_steps}_${desc}.txt
+slurm_array_file_path=${slurm_dir}/submission_files/${slurm_array_file}
+
+#finds the length of the array 
+num_XY_pos=$(ls $data_dir | awk "/.*XY.*/" | wc -l ) 
+
+array_length=1-${num_XY_pos}
+#max_jobs_at_once=%6
+
+mkdir -p ${slurm_dir}/out
+mkdir -p ${slurm_dir}/err
+mkdir -p ${slurm_dir}/submission_files
+
+if [[ -e $slurm_array_file_path ]]
+then
+	rm $slurm_array_file_path 
+fi
+
+touch $slurm_array_file_path
+
+#####################
+#GENERATE JOB SCRIPT#
+#####################
+
+for XY_pos in $(ls $data_dir | awk "/.*XY.*/"); do
+
+	img_path=${data_dir}/${XY_pos}
+	output_path=${out_dir}/${XY_pos}
+	position_label=$XY_pos
+
+	if [[ $pipeline_steps == 0-2 ]]
+	then
+
+		mkdir -p $output_path
+
+		echo bash ${script_dir}/run_pipe_0-2_single_position.sh $script_dir $tools_dir $data_dir $img_path $output_path $data_dir $position_label\
+					$reg_wts_file $icnn_am_wts_file $icnn_seg_wts_file >> $slurm_array_file_path
+
+	elif [[ $pipeline_steps == 3-9 ]]
+	then
+
+		echo bash ${script_dir}/run_pipe_3-9_single_position.sh $script_dir $tools_dir $data_dir $img_path $output_path $icnn_seg_wts_file >> $slurm_array_file_path
+
+	fi
+
+done
+
+#####################
+# SUBMIT JOB SCRIPT #
+#####################
+
+sbatch --output=${slurm_dir}/out/${desc}_${pipeline_steps}_XY%a.out \
+       --error=${slurm_dir}/err/${desc}_${pipeline_steps}_XY%a.err \
+       --array=${array_length}${max_jobs_at_once} \
+         ${script_dir}/submit_array_job_${pipeline_steps}.sh $slurm_array_file_path
+
---
+++ src/3-compute/slurm_scripts/submit_array_job_0-2.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+#                     Slurm Construction Section
+#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+# job name
+#SBATCH --job-name=xing-pipeline-segs
+
+#SBATCH --gres=gpu:1
+#SBATCH --exclude=g019,g102,g104,g122,g012,g013,g131
+#SBATCH --cpus-per-task=16
+
+#SBATCH --mem=40G
+
+# partition (queue) declaration
+#SBATCH --partition=any_gpu
+
+# number of requested nodes
+#SBATCH --nodes=1
+
+# number of tasks
+#SBATCH --ntasks=1
+
+# standard output & error
+
+#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+slurm_array_file_path=$1
+
+file_line=$(sed -n "$SLURM_ARRAY_TASK_ID"p $slurm_array_file_path)
+
+eval $file_line
+
+
+#sbatch --array=1-2 scripts/3_compute/run_pipe_0-2_all_positions_dante.sh
