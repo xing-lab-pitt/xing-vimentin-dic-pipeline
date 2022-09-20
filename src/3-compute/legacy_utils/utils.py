f6df80990d5c054a17dc67d25c04381acf600c25
user_name <dummtindex@gmail.com>
2022-09-20 10:10:11 -0400

    update package code

--- src/3-compute/legacy_utils/utils.py
+++ src/3-compute/legacy_utils/utils.py
@@ -17,8 +17,6 @@ import skimage.io
 import skimage.transform
 from .cellTool_utils import *
 from .io_utils import *
-from pims import ImageSequenceND
 from skimage.io import imread
 from skimage.measure import regionprops
-from skimage.restoration import (denoise_bilateral, denoise_nl_means,
-                                 denoise_wavelet, estimate_sigma)
+from skimage.restoration import denoise_bilateral, denoise_nl_means, denoise_wavelet, estimate_sigma
